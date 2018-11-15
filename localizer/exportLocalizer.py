5e3755584b8a08ea6b84b9f8893bddc889f968e5
isadenghien <isabelle.denghien@cea.fr>
2018-11-15 16:14:50 +0100

    first version for the localizer standard from eprime to expyriment

--- localizer/exportLocalizer.py
+++ localizer/exportLocalizer.py
@@ -6,33 +6,81 @@ Created on Tue Nov 13 10:22:08 2018
 @author: id983365
 """
 
-import csv
-import io
+import pandas as pd
 
-import pandas
 
-csv_file = '/neurospin/unicog/protocols/IRMf/New_localizers/localizer_standard/exportEprime.csv'
-#file_r = csv.reader(io.open(csv_file, 'r', encoding='utf-16-le'), delimiter='\t')
-##stimlist = csv.reader(io.open(listfile, 'r', encoding='utf-8-sig'), delimiter='\t')
-#encoding='utf-16-le'
-#for row in file_r :
-#    print(row)
+#Fixation cross all the time excepted during rsvp and pictseq
+#In order to take into account the original localizer in eprime
+#Launch the stimulation as follow:
+#python audiovis.py ./localizer/export_session1_localizer.csv --rsvp-display-time=250 --rsvp-display-isi=100 --picture-display-time=200 --picture-isi=0
 
-df = pandas.read_csv(csv_file, sep = '\t', header = 0, encoding ='utf-16-le')
-for i, r in enumerate(df.itertuples(), 1):
-#    if r['manip']
-  print(r[65]) # imprime le nom de la colonne
+condition_name = []
+type_stimulus = []
+stim = []
+
+csv_file = './localizer/exportEprime.csv'
+df = pd.read_csv(csv_file, sep = '\t', header = 0, encoding ='utf-16-le')
+for i, r_value in enumerate(df.itertuples(), 0):
+    manip = r_value[65] #corresponds to the 'manip' column
+    if manip == "calculs lus":
+        condition_name.append('calculvideo')
+        type_stimulus.append('rsvp')
+        stim.append('{},{},{},{}'.format(df.mot1[i], df.mot2[i], df.mot3[i], df.mot4[i]))
+    elif manip == "calculs entendus":
+       condition_name.append('calculaudio')
+       type_stimulus.append('sound')
+       stim.append(df.son[i])
+    elif manip == "clics D entendus":
+       condition_name.append('clicDaudio')
+       type_stimulus.append('sound')
+       stim.append(df.son[i])
+    elif manip == "clics G entendus":
+       condition_name.append('clicGaudio')
+       type_stimulus.append('sound')
+       stim.append(df.son[i])
+    elif manip == "clics D lus":
+       condition_name.append('clicDvideo')
+       type_stimulus.append('rsvp')
+       stim.append('{},{},{},{}'.format(df.mot1[i], df.mot2[i], df.mot3[i], df.mot4[i]))
+    elif manip == "clics G lus":
+       condition_name.append('clicGvideo')
+       type_stimulus.append('rsvp')
+       stim.append('{},{},{},{}'.format(df.mot1[i], df.mot2[i], df.mot3[i], df.mot4[i]))
+    elif manip == "damier H":
+       condition_name.append('CboardH')
+       type_stimulus.append('pictseq')
+       stim.append('checherboardHpb.bmp,checherboardHnb.bmp,checherboardHpb.bmp,checherboardHnb.bmp,checherboardHpb.bmp,checherboardHnb.bmp')
+    elif manip == "damier V":
+       condition_name.append('CboardV')
+       type_stimulus.append('pictseq')
+       stim.append('checherboardVpb.bmp,checherboardVnb.bmp,checherboardVpb.bmp,checherboardVnb.bmp,checherboardVpb.bmp,checherboardVnb.bmp')
+    elif manip == "phrases lues":
+       condition_name.append('phraseVideo')
+       type_stimulus.append('rsvp')
+       stim.append('{},{},{},{}'.format(df.mot1[i], df.mot2[i], df.mot3[i], df.mot4[i]))
+    elif manip == "phrases entendues":
+       condition_name.append('phraseAudio')
+       type_stimulus.append('sound')
+       stim.append(df.son[i])
+    else:
+       print('repos')
+       
+
+#EXPORT ONSET
+onset_file = './localizer/sub-01_task-standartloc_events.tsv'
+onset_df = pd.read_csv(onset_file, sep = '\t')
+onsets = onset_df['onset']*1000
+onsets_int=[]
+for onset in onsets:
+    onsets_int.append(int(onset))
+
+#CREATE THE CSV
+stim_data = {'condition_name': condition_name,
+        'onset': onsets_int,
+        'stype': type_stimulus,
+        'stimulus': stim}
+
+df = pd.DataFrame(stim_data)
+file_csv = './localizer/export_session1_localizer.csv'
+df.to_csv(file_csv, index=False, sep='\t', header=0)
   
-  
- csv_file = '/neurospin/unicog/protocols/IRMf/New_localizers/localizer_standard/exportEprime.csv'
-#file_r = csv.reader(io.open(csv_file, 'r', encoding='utf-16-le'), delimiter='\t')
-##stimlist = csv.reader(io.open(listfile, 'r', encoding='utf-8-sig'), delimiter='\t')
-#encoding='utf-16-le'
-#for row in file_r :
-#    print(row)
-
-df = pandas.read_csv(csv_file, sep = '\t', header = 0, encoding ='utf-16-le')
-for i, x in enumerate(df, 1):
-    print(i)
-    print(x) # imprime le nom de la colonne
- 
